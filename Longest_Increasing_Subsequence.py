3c0970db1f11cf83079ce8cdc215895f33082f87
Priyankajoshipj <33767244+Priyankajoshipj@users.noreply.github.com>
2019-08-25 21:49:11 -0500

    Dynammic Programming

---
+++ Longest_Increasing_Subsequence.py
@@ -0,0 +1,25 @@
+'''
+300. Longest Increasing Subsequence [Accepted]
+Input: [10,9,2,5,3,7,101,18]
+Output: [2,3,7,101] -> 4
+'''
+def lengthOfLIS(nums):
+	res = []
+    if not nums:
+        return 0
+    maxLIS = 1
+    res.append(1)
+    right = 1
+
+    while(right < len(nums)):
+        left = 0
+        count = 0
+        while(left < right):
+            if nums[left] < nums[right]:
+                count = max(count, res[left])
+            left += 1
+        res.append(count + 1)
+        maxLIS = max(res[right], maxLIS)
+        right += 1
+
+    return maxLIS
